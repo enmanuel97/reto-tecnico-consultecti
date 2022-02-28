63fe72b69ef959c900805d2bb3390ac0d250142d
Jesus Enmanuel De La Cruz <edelacruz9713@gmail.com>
2022-02-28 17:19:05 -0400

    Actualizacion

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { ICategories } from '../../interfaces/categories.interface';
+import { IProducts } from '../../interfaces/products.interface';
+import { forkJoin } from 'rxjs';
 
 @Component({
   	selector: 'app-home',
@@ -10,18 +12,23 @@ import { ICategories } from '../../interfaces/categories.interface';
 export class HomeComponent implements OnInit {
 
 	public categories: ICategories[] = [];
+	public products: IProducts[] = [];
 	public isLoading: boolean = true;
 
   	constructor(private apiService: ApiService) { }
 	
   	ngOnInit(): void {
+		this.getData();
   	}
 	
-	private getCategories() {
-		this.apiService.getCategories().subscribe((response) => {
-			this.categories = response;
-		}, (error: any) => {
-			console.log(error);
+	private getData() {
+		forkJoin([
+			this.apiService.getCategories(),
+			this.apiService.getProducts()
+		]).subscribe(response => {
+			this.categories = response[0];
+			this.products 	= response[1];
+			this.isLoading 	= false;
 		});
 	}
 }
